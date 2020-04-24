1700668c661c4b8472cd2e95b72d8d14935453ea
David Horstman <davidzilla27@gmail.com>
2020-04-24 17:58:11 -0400

    DRY out

--- friends/src/utils/axiosWithAuth.js
+++ friends/src/utils/axiosWithAuth.js
@@ -3,57 +3,6 @@ import axios from "axios";
 const baseURL = "http://localhost:5000/api/";
 
 /**
- * Create an axios instance which automatically includes
- * an authorization token specified by the "token" key in
- * localStorage as well as the ability to cancel the call
- * if the component unmounts.
- * This version expects the token to be a JSON-encoded string.
- * @param {object} [options] The options to pass on to axios.create()
- */
-export function axiosWithAuthCancellable(options) {
-  const token = JSON.parse(localStorage.getItem("token"));
-
-  let unmountedInternal = false;
-  let source = axios.CancelToken.source();
-  /**
-   * A function to cancel the API call in progress
-   * and prevent any downstream changes based on the unmounted() property.
-   * Call in componentWillUnmount() or return from useEffect().
-   */
-  const cancelAPICall = () => {
-    unmountedInternal = true;
-    source.cancel("Component unmounted. Data fetching cancelled.");
-  };
-  /**
-   * A function to perform axios calls which automatically include
-   * an authorization token specified by the "token" key in
-   * localStorage as well as the ability to cancel the call
-   * if the component unmounts using cancelAPICall().
-   * This version expects the token to be a JSON-encoded string.
-   * Specify any additional options when calling the parent function.
-   */
-  const axiosWithAuth = () =>
-    axios.create({
-      baseURL,
-      cancelToken: source.token,
-      headers: {
-        ...options?.headers,
-        Authorization: token,
-      },
-      ...options,
-    });
-
-  return {
-    isCancel: axios.isCancel,
-    /**
-     * @returns {boolean} Whether the component has been unmounted
-     */
-    unmounted: () => unmountedInternal,
-    axiosWithAuth,
-    cancelAPICall,
-  };
-}
-/**
  * A function to perform axios calls which automatically include
  * an authorization token specified by the "token" key in
  * localStorage.
@@ -82,6 +31,48 @@ export function axiosWithoutAuth(options) {
     baseURL,
     ...options,
   });
+}
+/**
+ * Create an axios instance which automatically includes
+ * an authorization token specified by the "token" key in
+ * localStorage as well as the ability to cancel the call
+ * if the component unmounts.
+ * This version expects the token to be a JSON-encoded string.
+ * @param {object} [options] The options to pass on to axios.create()
+ */
+export function axiosWithAuthCancellable(options) {
+  let source = axios.CancelToken.source();
+  let unmountedInternal = false;
+  /**
+   * @returns {boolean} Whether the component has been unmounted
+   */
+  const unmounted = () => unmountedInternal;
+  /**
+   * A function to cancel the API call in progress and
+   * prevent any downstream changes based on the unmounted() property.
+   * Call in componentWillUnmount() or return from useEffect().
+   */
+  const cancelAPICall = () => {
+    unmountedInternal = true;
+    source.cancel("Component unmounted. Data fetching cancelled.");
+  };
+  /**
+   * A function to perform axios calls which automatically include
+   * an authorization token specified by the "token" key in
+   * localStorage as well as the ability to cancel the call
+   * if the component unmounts using cancelAPICall().
+   * This version expects the token to be a JSON-encoded string.
+   * @param {object} [options] The options to be passed to axios.create()
+   */
+  const axiosWithAuthC = (optionsInner = options) =>
+    axiosWithAuth({ ...optionsInner, cancelToken: source.token });
+
+  return {
+    isCancel: axios.isCancel,
+    unmounted,
+    axiosWithAuth: axiosWithAuthC,
+    cancelAPICall,
+  };
 }
 
 export default axiosWithAuth;
