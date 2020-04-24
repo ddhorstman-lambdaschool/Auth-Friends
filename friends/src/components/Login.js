39f6e211b667ae5ff40e7ae7e9f7a65b81ca056f
David Horstman <davidzilla27@gmail.com>
2020-04-24 00:04:22 -0400

    Login Form

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,4 +1,77 @@
-import React from 'react';
-export default function Login(){
-    return <div />;
+import React from "react";
+import {
+    Container,
+    Card,
+    Button,
+    TextField,
+    Typography,
+    ButtonGroup,
+} from "@material-ui/core";
+
+import axiosWithAuth from "../utils/axiosWithAuth";
+
+const initialState = {
+    username: "",
+    password: "",
+};
+
+export default class Login extends React.Component {
+    state = initialState;
+
+    handleChange = ({ target: { name, value } }) => {
+        this.setState({ [name]: value });
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+        axiosWithAuth
+            .post("login", this.state)
+            //.then(r => {console.log(r); return r;})
+            .then(res => window.localStorage.setItem("token", res.data.payload))
+            .then(() => this.props.history.push("/friends"))
+            .catch(console.error);
+    };
+
+    handleReset = e => {
+        e && e.preventDefault();
+        this.setState(initialState);
+    };
+    render() {
+        return (
+            <Container>
+                <Card style={{ margin: "auto", width: "50%", padding: "10px" }}>
+                    <form
+                        style={{ display: "flex", flexDirection: "column" }}
+                        onSubmit={this.handleSubmit}
+                        onReset={this.handleReset}
+                    >
+                        <Typography variant="h3">Log in</Typography>
+                        <TextField
+                            required
+                            label="Username"
+                            name="username"
+                            value={this.state.username}
+                            onChange={this.handleChange}
+                        />
+                        <TextField
+                            required
+                            label="Password"
+                            name="password"
+                            type="password"
+                            value={this.state.password}
+                            onChange={this.handleChange}
+                        />
+                        <ButtonGroup>
+                            <Button variant="contained" type="submit" color="primary">
+                                Submit
+                            </Button>
+                            <Button variant="contained" type="reset">
+                                Reset
+                            </Button>
+                        </ButtonGroup>
+                    </form>
+                </Card>
+            </Container>
+        );
+    }
 }
